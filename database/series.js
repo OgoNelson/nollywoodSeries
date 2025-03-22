1ed911a51677fca00005b1018372eb9061480938
Ogo Nelson <goldnelsgroup@gmail.com>
2025-03-23 00:36:14 +0100

    feat: set up the server and the rendering based on path

--- database/series.js
+++
@@ -1,8 +0,0 @@
-const series = [
-  { id: 1, name: Kabiyesi, rating: 4.5, plot: t, seasons: 1 },
-  { id: 1, name: Kabiyesi, rating: 4.5, plot: t, seasons: 1 },
-  { id: 1, name: Kabiyesi, rating: 4.5, plot: t, seasons: 1 },
-  { id: 1, name: Kabiyesi, rating: 4.5, plot: t, seasons: 1 },
-];
-
-module.export = { series };
